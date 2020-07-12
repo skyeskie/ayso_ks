93bc6728901db5a517e6138849df7b6d141b5fd4
Scott Yeskie <scott@yeskie.com>
2020-07-11 21:53:24 -0500

    Implement field view (basic)

--- lib/module/region/field_view.dart
+++ lib/module/region/field_view.dart
@@ -1,16 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
+import '../../models/region.dart';
 import '../../widgets/nav_bar.dart';
 
 class FieldView extends StatelessWidget {
-  const FieldView(this.regionNum);
+  FieldView(this.regionNum) : mapName = Region.fromNumber(regionNum).mapFile;
 
   final int regionNum;
+  final String mapName;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: buildNavBar('Region $regionNum', context),
+      appBar: buildNavBar('Region $regionNum Fields', context),
+      body: SvgPicture.asset(mapName),
     );
   }
 }
