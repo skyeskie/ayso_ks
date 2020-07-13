024c58840178c370041c0b6d1722748611e4675b
Scott Yeskie <scott@yeskie.com>
2020-07-12 22:01:05 -0500

    Add sample for map view

--- lib/module/region/field_view.dart
+++ lib/module/region/field_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:photo_view/photo_view.dart';
 
 import '../../models/region.dart';
 import '../../widgets/nav_bar.dart';
@@ -14,7 +15,15 @@ class FieldView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: buildNavBar('Region $regionNum Fields', context),
-      body: SvgPicture.asset(mapName),
+      body: PhotoView.customChild(
+        child: SvgPicture.asset(
+          mapName,
+          height: 1000,
+          width: 1000,
+        ),
+        childSize: Size(1000, 1000),
+        backgroundDecoration: BoxDecoration(color: Colors.white),
+      ),
     );
   }
 }
--- lib/module/region/street_map_view.dart
+++ lib/module/region/street_map_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:photo_view/photo_view.dart';
 
 import '../../widgets/nav_bar.dart';
 
@@ -11,6 +12,18 @@ class StreetMapView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: buildNavBar('Region $regionNum', context),
+      body: PhotoView(
+        imageProvider: AssetImage('img/osm_region49_1500sq.png'),
+        initialScale: 1.0,
+        minScale: 1.0,
+        maxScale: 1.0,
+        basePosition: Alignment(0.0, -0.9),
+      ),
+      floatingActionButton: RaisedButton.icon(
+        onPressed: () {},
+        icon: Icon(Icons.navigation),
+        label: Text('DUMMY MAP - ONLY STRIKER'),
+      ),
     );
   }
 }
